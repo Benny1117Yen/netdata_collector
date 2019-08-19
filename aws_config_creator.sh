5cef7374c28c60349d513bf3491762484acfa71a
P86071244 <P86071244@gs.ncku.edu.tw>
2019-08-19 08:56:49 +0000

    Directory .aws copy from lib to cache

---
+++ aws_config_creator.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+directory_name=/var/cache/netdata/.aws
+directory_name=${directory_name}
+
+if [ ! -d "$directory_name" ]; then
+    echo "Directory making $directory_name"
+	cp -a /var/lib/.aws "$directory_name"
+fi
